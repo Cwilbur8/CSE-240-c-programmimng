4e501914ec2d9401b781e700c0d172b86a771e5b
Christian Wilburn <cwilbur8@asu.edu>
2026-04-23 09:33:57 -0700

    HW1 intro to Debugging

---
+++ hw01q3.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+int main() {
+char ch = '+';
+int a = 10, b = 20;
+double f;
+printf("ch = %c\n", ch);
+switch (ch) {
+case '+': f = a + b; printf("f = %f\n", f);
+break;
+case '-': f = a - b; printf("f = %f\n", f);
+break;
+case '*': f = a * b; printf("f = %f\n", f);
+break;
+case '/': f = a / b; printf("f = %f\n", f);
+break;
+default: printf("invalid operator\n");
+}
+ch = '-';
+printf("ch = %c\n", ch);
+switch (ch) {
+case '+': f = a + b; printf("f = %f\n", f);
+break;
+case '-': f = a - b; printf("f = %f\n", f);
+break;
+case '*': f = a * b; printf("f = %f\n", f);
+break;
+case '/': f = a / b; printf("f = %f\n", f);
+break;
+default: printf("invalid operator\n");
+}
+ch = '*';
+printf("ch = %c\n", ch);
+switch (ch) {
+case '+': f = a + b; printf("f = %f\n", f);
+break;
+case '-': f = a - b; printf("f = %f\n", f);
+break;
+case '*': f = a * b; printf("f = %f\n", f);
+break;
+case '/': f = a / b; printf("f = %f\n", f);
+break;
+default: printf("invalid operator\n");
+}
+ch = '/';
+printf("ch = %c\n", ch);
+switch (ch) {
+case '+': f = a + b; printf("f = %f\n", f);
+break;
+case '-': f = a - b; printf("f = %f\n", f);
+break;
+case '*': f = a * b; printf("f = %f\n", f);
+break;
+case '/': f = a / b; printf("f = %f\n", f);
+break;
+default: printf("invalid operator\n");
+}
+ch = '%';
+printf("ch = %c\n", ch); switch (ch) {
+case '+': f = a + b; printf("f = %f\n", f);
+break;
+case '-': f = a - b; printf("f = %f\n", f);
+break;
+case '*': f = a * b; printf("f = %f\n", f);
+break;
+case '/': f = a / b; printf("f = %f\n", f);
+break;
+default: printf("invalid operator\n");
+}
+return ch;
+}
